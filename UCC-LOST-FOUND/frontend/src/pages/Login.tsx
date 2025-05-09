e7f59ccd0415c8d6feb123eb6117d8704e322c3f
SKYRENZ <131676044+SKYRENZ@users.noreply.github.com>
2025-05-09 16:47:05 +0800

    Login page
    added a card container for visibility and layout

--- UCC-LOST-FOUND/frontend/src/pages/Login.tsx
+++ UCC-LOST-FOUND/frontend/src/pages/Login.tsx
@@ -1,13 +1,12 @@
-
 import '../css/Login.css';
 
 function Login() {
   return (
     <div className="login-container">
-      <h1 className="login-title">Log In</h1>
+      <h1 className="login-title">Welcome Back</h1>
       <form className="login-form">
-        <input type="email" placeholder="Email" className="login-input" />
-        <input type="password" placeholder="Password" className="login-input" />
+        <input type="email" placeholder="Enter your email" className="login-input" />
+        <input type="password" placeholder="Enter your password" className="login-input" />
         <button type="submit" className="login-button">Log In</button>
       </form>
       <button className="facebook-login-button">Log In with Facebook</button>
